3ba00b8e3f36dfa1cbd0c21a8fd8094b1c5869a7
limbersMay <limbermay1@gmail.com>
2022-08-03 14:05:02 -0500

    se agregó la función de change directory para cambiar de directorio

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
 #!/usr/bin/python
-
+import os
 import argparse
+import sys
 from pathlib import Path
+from helpers.fichero import Fichero
+
+
+def changeDirectory(fichero: Fichero, path: str):
+    # If we received the path
+
+    # Check if the path exists in our list of paths
+    try:
+        path = fichero.obtener_valor(path_to_dir)
+        os.chdir(path)
+        os.system("usr/bin/zsh")
+
+        return
+
+    except KeyError:
+        pass
+
+    if Path(path_to_dir).exists():
+        os.chdir(path_to_dir)
+        os.system("/usr/bin/zsh")
+
+    if not Path(path_to_dir).exists():
+        print(f"{path_to_dir} does not exist.")
+        sys.exit(1)
+
+def process_args(path_to_dir: Path):
+
+    fichero = Fichero('config/directories.json')
+
+    if path_to_dir:
+        changeDirectory(fichero, path_to_dir)
+        return
+
 
 def cli() -> argparse.Namespace:
 
@@ -11,8 +45,8 @@ def cli() -> argparse.Namespace:
     )
 
     parser.add_argument(
-        '--path-to-dir',
-        type=Path,
+        '-c', '--change',
+        type=str,
         help='Path to change to.',
     )
 
@@ -24,7 +58,7 @@ def cli() -> argparse.Namespace:
 
     parser.add_argument(
         '-p', '--path',
-        type=Path,
+        type=str,
         help='Path to register.'
     )
 
@@ -37,7 +71,7 @@ def cli() -> argparse.Namespace:
 
     return parser.parse_args()
 
-import os
+
 if __name__ == '__main__':
     # os.chdir("/home/limbers/Documents")
     # os.system("pwd")
@@ -45,5 +79,4 @@ if __name__ == '__main__':
 
     args = cli()
 
-    print(args.name)
-    print(args.path)
+    process_args(args.change)
