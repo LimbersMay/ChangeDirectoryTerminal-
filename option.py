4a813b56ef398754fdcbc79eb111d93f45590e2a
limbersMay <limbermay1@gmail.com>
2022-08-31 16:56:56 -0500

    se creó la clase opción para guardar todas las opciones

---
+++ option.py
@@ -0,0 +1,28 @@
+class Option:
+    
+    def init(self, switch, name, path, delete, list_paths, register):
+        self.switch = switch # Path to switch to
+        self.name = name # Name to the path to register
+        self.path = path # Path to register
+        self.delete = delete # Name of the path to delete
+        self.list_paths = list_paths # Check if the user want to list all the paths in the json 
+        
+        self.register = register # Check if the user want to register the actual path
+
+    def getSwitch(self):
+        return self.switch
+
+    def getName(self):
+        return self.name
+
+    def getPath(self):
+        return self.path 
+    
+    def getDelete(self):
+        return self.delete
+
+    def getListPaths(self):
+        return self.list_paths
+    
+    def getRegister(self):
+        return self.register
